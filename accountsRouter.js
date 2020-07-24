163559b116640d17890acea4832c9873a6bcfd47
ryanp <ryanpanimation@gmail.com>
2020-07-23 23:42:54 -0500

    delete successful

--- accountsRouter.js
+++ accountsRouter.js
@@ -48,11 +48,22 @@ router.put("/:id", validateAccountBody, async (req, res, next) => {
   }
 });
 
+router.delete("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.accountInfo;
+    const data = await db("accounts").where("id", id).del();
+    res.status(200).json({ message: `Account ${id} deleted` });
+  } catch (error) {
+    console.log(error);
+    next({ status: 500, errorMessage: "Unable to delete account" });
+  }
+});
+
 async function validateAccountId(req, res, next) {
   const { id } = req.params;
   // const accountId = await db.get(id);
   const accountId = await db("accounts").where("id", id);
-  if (accountId) {
+  if (accountId.length !== 0) {
     req.accountInfo = accountId[0];
     next();
   } else next({ status: 404, errorMessage: "Invalid ID" });
